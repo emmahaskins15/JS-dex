338ca1868644a1a13e063ff915a6b7c203e2c27b
Emma Haskins <emmahaskins15@gmail.com>
2019-06-12 13:58:34 +0200

    Implemented repository and output

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,19 @@
-alert('Hello world');
+var repository = [
+  {name: 'Bulbasaur', height: 70, types: ['grass', 'poison']},
+  {name: 'Ivysaur', height: 100, types: ['grass', 'poison']},
+  {name: 'Venusaur', height: 200, types: ['grass', 'poison']}
+];
+for (var i = 0; i < repository.length; i++) {
+  if (repository[i].height < 200){
+  document.write('Name: '+repository[i].name+'<br>');
+  document.write('Height: '+repository[i].height+'cm<br>');
+  document.write('Types: '+repository[i].types+'<br>');
+  document.write('<br>');
+}
+  else {
+    document.write('Name: '+repository[i].name+'<br>');
+    document.write('Height: '+repository[i].height+'cm - Wow! That\'s big!<br>');
+    document.write('Types: '+repository[i].types+'<br>');
+    document.write('<br>');
+      }
+}
