33147ff9df3c30c6399bb25e0fde07b7ce9e748b
Emma Haskins <emmahaskins15@gmail.com>
2019-06-12 14:21:25 +0200

    Added spaces between 'Types' output

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,20 @@
-var repository = [
+var dex = [
   {name: 'Bulbasaur', height: 70, types: ['grass', 'poison']},
   {name: 'Ivysaur', height: 100, types: ['grass', 'poison']},
   {name: 'Venusaur', height: 200, types: ['grass', 'poison']}
 ];
-for (var i = 0; i < repository.length; i++) {
-  if (repository[i].height < 200){
-  document.write('Name: '+repository[i].name+'<br>');
-  document.write('Height: '+repository[i].height+'cm<br>');
-  document.write('Types: '+repository[i].types+'<br>');
+
+for (var i = 0; i < dex.length; i++) {
+  if (dex[i].height < 200){
+  document.write('Name: '+dex[i].name+'<br>');
+  document.write('Height: '+dex[i].height+'cm<br>');
+  document.write('Types: '+dex[i].types.join(', ')+'<br>');
   document.write('<br>');
 }
   else {
-    document.write('Name: '+repository[i].name+'<br>');
-    document.write('Height: '+repository[i].height+'cm - Wow! That\'s big!<br>');
-    document.write('Types: '+repository[i].types+'<br>');
+    document.write('Name: '+dex[i].name+'<br>');
+    document.write('Height: '+dex[i].height+'cm - Wow! That\'s big!<br>');
+    document.write('Types: '+dex[i].types.join(', ')+'<br>');
     document.write('<br>');
       }
 }
