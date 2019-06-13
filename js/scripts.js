035ad10df3b863a127f48ff2c5786d444962ed39
Emma Haskins <emmahaskins15@gmail.com>
2019-06-13 19:00:43 +0200

    Added comments and refactored for loop
    Refactored for loop with forEach method

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,23 @@
+//Declaring Pokedex database
 var dex = [
   {name: 'Bulbasaur', height: 70, types: ['grass', 'poison']},
   {name: 'Ivysaur', height: 100, types: ['grass', 'poison']},
   {name: 'Venusaur', height: 200, types: ['grass', 'poison']}
 ];
 
-for (var i = 0; i < dex.length; i++) {
-  if (dex[i].height < 200){
-  document.write('Name: '+dex[i].name+'<br>');
-  document.write('Height: '+dex[i].height+'cm<br>');
-  document.write('Types: '+dex[i].types.join(', ')+'<br>');
-  document.write('<br>');
-}
-  else {
-    document.write('Name: '+dex[i].name+'<br>');
-    document.write('Height: '+dex[i].height+'cm - Wow! That\'s big!<br>');
-    document.write('Types: '+dex[i].types.join(', ')+'<br>');
+//Listing contents of Pokemon database
+dex.forEach(function(property) {
+    if (property.height < 200){
+    document.write('Name: '+property.name+'<br>');
+    document.write('Height: '+property.height+'cm<br>');
+    document.write('Types: '+property.types.join(', ')+'<br>');
     document.write('<br>');
-      }
-}
+  }
+    else {
+      document.write('Name: '+property.name+'<br>');
+      document.write('Height: '+property.height+'cm - Wow! That\'s big!<br>');
+      document.write('Types: '+property.types.join(', ')+'<br>');
+      document.write('<br>');
+        }
+  }
+)
